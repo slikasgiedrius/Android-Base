5357c1c19f278ddde0eb0a1211ef2a184d4a1051
Giedrius Šlikas <slikas.giedrius@gmail.com>
2022-09-09 12:52:51 +0300

    Add Compose base

---
+++ app/src/main/java/com/giedrius/slikas/androidbase/compose/base/Color.kt
@@ -0,0 +1,9 @@
+package com.giedrius.slikas.androidbase.compose.base
+
+import androidx.compose.ui.graphics.Color
+
+val Beige = Color(0xF5F5F2)
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
